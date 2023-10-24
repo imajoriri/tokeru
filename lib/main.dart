1b58cff317ddb1721721ce6845c2a811e6606a25
imajoriri <imajo.srroni@gmail.com>
2023-10-24 09:54:48 +0900

    sharedやめる

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-// import 'package:native_shared_preferences/original_shared_preferences/original_shared_preferences.dart';
 
 void main() {
   runApp(
