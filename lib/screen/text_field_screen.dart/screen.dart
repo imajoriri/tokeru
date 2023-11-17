f5b7f9c9cfe847172627c59856ae9122734d4c53
imajoriri <imajo.srroni@gmail.com>
2023-11-17 18:28:19 +0900

    Refactor text field screen widget

--- lib/screen/text_field_screen.dart/screen.dart
+++ lib/screen/text_field_screen.dart/screen.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:collection/collection.dart';
-import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -17,24 +16,12 @@ class TextFieldScreen extends HookConsumerWidget {
 
   final globalKey = GlobalKey();
 
-  void updateWindowSize(BuildContext context) {
-    RenderBox? box = globalKey.currentContext?.findRenderObject() as RenderBox?;
-    final size = box?.size;
-    if (size != null) {
-      final height = size.height;
-      // これ他おそらくmainを変えてしまっている
-      DesktopWindow.setWindowSize(
-          Size(MediaQuery.of(context).size.width, height + 50));
-    }
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     const channel = MethodChannel("net.cbtdev.sample/method");
     channel.setMethodCallHandler((MethodCall call) async {
       switch (call.method) {
         case 'openPanel':
-          updateWindowSize(context);
           break;
         default:
           break;
@@ -46,89 +33,71 @@ class TextFieldScreen extends HookConsumerWidget {
         .map((d) => useMarkdownTextEditingController(text: d.content))
         .toList();
 
-    // useEffect(() {
-    //   WidgetsBinding.instance.addPostFrameCallback((_) async {
-    //     // await ref.watch(draftStoreProvider.future);
-    //     // updateWindowSize(context);
-    //     // memoStoreProviderのaddMemoでstateがないと言われるのであらかじめ読んでおく
-    //     ref.watch(memoStoreProvider);
-    //   });
-    //   return null;
-    // }, []);
     return Material(
       // color: Colors.transparent,
       child: SingleChildScrollView(
-        child: Column(
-          key: globalKey,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Theme.of(context).colorScheme.surface,
-                border: Border.all(
-                  color: context.colorScheme.outline,
-                  width: 1.0,
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: context.colorScheme.shadow,
-                    spreadRadius: 0,
-                    blurRadius: 1,
-                    offset: const Offset(0, 1),
-                  ),
-                ],
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // draft
-                  ...controllers.mapIndexed((index, c) {
-                    if (drafts[index].content != c.text) {
-                      c.text = drafts[index].content;
-                    }
-                    return ChatDraftTextField(
-                      textController: c,
-                      defaultValue: c.text,
-                      onDebounceChanged: (value) {
-                        ref.read(draftControllerProvider.notifier).updateDraft(
-                              id: drafts[index].id,
-                              content: value,
-                            );
-                      },
-                      onSubmit: (value) async {
-                        await ref
-                            .read(memoStoreProvider.notifier)
-                            .addMemo(content: value, isBookmark: false);
-                        ref
-                            .read(draftControllerProvider.notifier)
-                            .removeDraft(drafts[index].id);
-                      },
-                    );
-                  }),
-
-                  // main
-                  ChatMainTextField(
-                    onChanged: (value) {
-                      updateWindowSize(context);
-                    },
-                    onAddDraft: (value) async {
-                      await ref
-                          .read(draftControllerProvider.notifier)
-                          .addDraft(value);
-                      updateWindowSize(context);
-                    },
-                    onSubmit: (value) async {
-                      await ref
-                          .read(memoStoreProvider.notifier)
-                          .addMemo(content: value, isBookmark: false);
-                      updateWindowSize(context);
-                    },
-                  ),
-                ],
-              ),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Theme.of(context).colorScheme.surface,
+            border: Border.all(
+              color: context.colorScheme.outline,
+              width: 1.0,
             ),
-          ],
+            boxShadow: [
+              BoxShadow(
+                color: context.colorScheme.shadow,
+                spreadRadius: 0,
+                blurRadius: 1,
+                offset: const Offset(0, 1),
+              ),
+            ],
+          ),
+          child: Column(
+            key: globalKey,
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              // draft
+              ...controllers.mapIndexed((index, c) {
+                if (drafts[index].content != c.text) {
+                  c.text = drafts[index].content;
+                }
+                return ChatDraftTextField(
+                  textController: c,
+                  defaultValue: c.text,
+                  onDebounceChanged: (value) {
+                    ref.read(draftControllerProvider.notifier).updateDraft(
+                          id: drafts[index].id,
+                          content: value,
+                        );
+                  },
+                  onSubmit: (value) async {
+                    await ref
+                        .read(memoStoreProvider.notifier)
+                        .addMemo(content: value, isBookmark: false);
+                    ref
+                        .read(draftControllerProvider.notifier)
+                        .removeDraft(drafts[index].id);
+                  },
+                );
+              }),
+
+              // main
+              ChatMainTextField(
+                onAddDraft: (value) async {
+                  await ref
+                      .read(draftControllerProvider.notifier)
+                      .addDraft(value);
+                },
+                onSubmit: (value) async {
+                  await ref
+                      .read(memoStoreProvider.notifier)
+                      .addMemo(content: value, isBookmark: false);
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
