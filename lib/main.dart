6e83db6ec6ba619c70467d078d2cd2e99e0da57a
imajoriri <imajo.srroni@gmail.com>
2024-02-08 17:35:41 +0900

    observer追加

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:logger/logger.dart';
 import 'package:quick_flutter/firebase_options.dart';
 import 'package:quick_flutter/screen/main_memo/screen.dart';
 import 'package:quick_flutter/screen/text_field_screen.dart/screen.dart';
@@ -15,6 +16,7 @@ void main() async {
 
   runApp(
     ProviderScope(
+      observers: [_AppObserver()],
       child: AppMaterialApp(
         home: const MainMemoScreen(),
       ),
@@ -31,11 +33,46 @@ void panel() async {
 
   runApp(
     ProviderScope(
+      observers: [_AppObserver()],
       child: AppMaterialApp(
         home: const TextFieldScreen(),
       ),
     ),
   );
+}
+
+class _AppObserver extends ProviderObserver {
+  @override
+  void didUpdateProvider(
+    ProviderBase<Object?> provider,
+    Object? previousValue,
+    Object? newValue,
+    ProviderContainer container,
+  ) {
+    if (newValue is AsyncError) {
+      final logger = Logger();
+      logger.e(
+        newValue.error.toString(),
+        error: newValue.error,
+        stackTrace: newValue.stackTrace,
+      );
+    }
+  }
+
+  @override
+  void providerDidFail(
+    ProviderBase<Object?> provider,
+    Object error,
+    StackTrace stackTrace,
+    ProviderContainer container,
+  ) {
+    final logger = Logger();
+    logger.e(
+      error,
+      error: error,
+      stackTrace: stackTrace,
+    );
+  }
 }
 
 // class MyApp extends StatefulHookConsumerWidget {
