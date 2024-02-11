46a87233d46be5bf47d40f5991ae7d1640bcd0d4
kakudenbuzo <kakudenbuzou@gmail.com>
2024-02-11 15:40:24 +0900

    Add JSON serialization and deserialization for Todo class

--- lib/model/todo/todo.dart
+++ lib/model/todo/todo.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'todo.freezed.dart';
+part 'todo.g.dart';
 
 @freezed
 class Todo with _$Todo {
@@ -10,6 +12,19 @@ class Todo with _$Todo {
     required bool isDone,
     required int indentLevel,
     required int index,
-    required DateTime createdAt,
+    @TimestampConverter() required DateTime createdAt,
   }) = _Todo;
+  factory Todo.fromJson(Map<String, dynamic> json) => _$TodoFromJson(json);
+}
+
+class TimestampConverter implements JsonConverter<DateTime, Timestamp> {
+  const TimestampConverter();
+
+  @override
+  DateTime fromJson(Timestamp timestamp) {
+    return timestamp.toDate();
+  }
+
+  @override
+  Timestamp toJson(DateTime date) => Timestamp.fromDate(date);
 }
