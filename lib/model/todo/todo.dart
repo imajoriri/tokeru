4c054dac9d2d4ddc6538ccc4adf93dec8a23e9af
kakudenbuzo <kakudenbuzou@gmail.com>
2024-02-11 19:57:12 +0900

    Add TimestampConverter class for converting timestamps

--- lib/model/todo/todo.dart
+++ lib/model/todo/todo.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:quick_flutter/systems/timestamp_converter.dart';
 
 part 'todo.freezed.dart';
 part 'todo.g.dart';
@@ -15,16 +16,4 @@ class Todo with _$Todo {
     @TimestampConverter() required DateTime createdAt,
   }) = _Todo;
   factory Todo.fromJson(Map<String, dynamic> json) => _$TodoFromJson(json);
-}
-
-class TimestampConverter implements JsonConverter<DateTime, Timestamp> {
-  const TimestampConverter();
-
-  @override
-  DateTime fromJson(Timestamp timestamp) {
-    return timestamp.toDate();
-  }
-
-  @override
-  Timestamp toJson(DateTime date) => Timestamp.fromDate(date);
 }
