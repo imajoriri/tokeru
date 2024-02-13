8a8f55f11c274064bb218d721bdf552c543019ff
imajoriri <imajo.srroni@gmail.com>
2024-02-13 14:22:52 +0900

    feat: アプリのメインウィンドウをNSWindowに変更する

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -4,6 +4,7 @@ import FlutterMacOS
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
+    // falseにすることで、closeされてもバックグラウンドで動き続ける(アプリが終了しない)
+    return false
   }
 }
