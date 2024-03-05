03dff26dddd047729a8eb528df2ed2946a690376
imajoriri <imajo.srroni@gmail.com>
2024-03-05 09:54:26 +0900

    small windowを削除

--- lib/screen/text_field_screen/screen.dart
+++ lib/screen/text_field_screen/screen.dart
@@ -16,8 +16,6 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:super_hot_key/super_hot_key.dart';
 
 part 'screen.g.dart';
-part 'large_window.dart';
-part 'small_window.dart';
 part 'todo_list.dart';
 part 'memo.dart';
 
@@ -41,7 +39,6 @@ class TextFieldScreen extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final channel = ref.watch(methodChannelProvider);
-    final windowSizeMode = ref.watch(windowSizeModeControllerProvider);
     final bookmark = ref.watch(bookmarkControllerProvider);
 
     HotKey.create(
@@ -58,14 +55,9 @@ class TextFieldScreen extends HookConsumerWidget {
 
     ref.listen(windowSizeModeControllerProvider, (previous, next) {
       final _ = switch (next) {
-        // TODO: ここもビルド後にする??
         WindowSizeMode.small => {
-            ref.read(methodChannelProvider).invokeMethod(
-              AppMethodChannel.setFrameSize.name,
-              {"height": 50},
-            ),
+            FocusScope.of(context).unfocus(),
           },
-        // .largeになるときは `_LargeWindow` のコールバックでサイズを変えるので不要
         WindowSizeMode.large => {},
       };
     });
@@ -83,38 +75,128 @@ class TextFieldScreen extends HookConsumerWidget {
 
     return Material(
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            switch (windowSizeMode) {
-              WindowSizeMode.small => _SmallWindow(),
-              WindowSizeMode.large =>
-                NotificationListener<SizeChangedLayoutNotification>(
-                  onNotification: (notification) {
-                    // サイズが変更されたことを検知した時の処理
-                    ref.read(methodChannelProvider).invokeMethod(
-                      AppMethodChannel.setFrameSize.name,
-                      {"height": largeWindowKey.currentContext?.size?.height},
-                    );
-                    return true;
+        child: NotificationListener<SizeChangedLayoutNotification>(
+          onNotification: (notification) {
+            // サイズが変更されたことを検知した時の処理
+            ref.read(methodChannelProvider).invokeMethod(
+              AppMethodChannel.setFrameSize.name,
+              {"height": largeWindowKey.currentContext?.size?.height},
+            );
+            return true;
+          },
+          child: SizeChangedLayoutNotifier(
+            child: _LargeWindow(
+              key: largeWindowKey,
+              onBuildCallback: () {
+                ref.read(methodChannelProvider).invokeMethod(
+                  AppMethodChannel.setFrameSize.name,
+                  {
+                    "height": largeWindowKey.currentContext?.size?.height,
                   },
-                  child: SizeChangedLayoutNotifier(
-                    child: _LargeWindow(
-                      key: largeWindowKey,
-                      onBuildCallback: () {
-                        ref.read(methodChannelProvider).invokeMethod(
-                          AppMethodChannel.setFrameSize.name,
-                          {
-                            "height":
-                                largeWindowKey.currentContext?.size?.height,
-                          },
-                        );
-                      },
-                    ), // サイズ変更を監視したいウィジェット
+                );
+              },
+            ), // サイズ変更を監視したいウィジェット
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _LargeWindow extends HookConsumerWidget {
+  const _LargeWindow({Key? key, this.onBuildCallback}) : super(key: key);
+
+  /// ビルド後に呼ばれるコールバック
+  final Function? onBuildCallback;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final channel = ref.watch(methodChannelProvider);
+    final bookmark = ref.watch(bookmarkControllerProvider);
+    final windowSizeMode = ref.watch(windowSizeModeControllerProvider);
+
+    useEffect(
+      () {
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          onBuildCallback?.call();
+        });
+        return null;
+      },
+      const [],
+    );
+
+    return Padding(
+      padding: const EdgeInsets.only(top: 2.0, left: 4, right: 4),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (windowSizeMode == WindowSizeMode.large)
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          channel.invokeMethod(
+                            AppMethodChannel.openOrClosePanel.name,
+                          );
+                        },
+                        icon: const Icon(Icons.close),
+                      ),
+                    ],
                   ),
                 ),
-            },
-          ],
-        ),
+                // Dragできるようなアイコン
+                const MouseRegion(
+                  cursor: SystemMouseCursors.grabbing,
+                  child: Icon(Icons.drag_indicator_outlined),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          channel
+                              .invokeMethod(AppMethodChannel.windowToLeft.name);
+                        },
+                        icon: const Icon(Icons.arrow_circle_left_outlined),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          ref
+                              .read(bookmarkControllerProvider.notifier)
+                              .toggle();
+                        },
+                        tooltip: 'Window does not shrink when inactive',
+                        icon: Icon(
+                          bookmark ? Icons.push_pin : Icons.push_pin_outlined,
+                        ),
+                        color: bookmark
+                            ? context.colorScheme.primary
+                            : context.colorScheme.secondary,
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          channel.invokeMethod(
+                            AppMethodChannel.windowToRight.name,
+                          );
+                        },
+                        icon: const Icon(Icons.arrow_circle_right_outlined),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+
+          const TodoList(),
+          // 初期リリースでは非表示
+          // _MemoScreen(),
+        ],
       ),
     );
   }
