775bbdc159333b8694f4adca4fd6d816cf706d12
imajoriri <imajo.srroni@gmail.com>
2024-03-08 16:36:26 +0900

    screenのリファクタ

--- lib/screen/text_field_screen/screen.dart
+++ lib/screen/text_field_screen/screen.dart
@@ -111,10 +111,6 @@ class _LargeWindow extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final channel = ref.watch(methodChannelProvider);
-    final bookmark = ref.watch(bookmarkControllerProvider);
-    // final windowSizeMode = ref.watch(windowSizeModeControllerProvider);
-
     useEffect(
       () {
         WidgetsBinding.instance.addPostFrameCallback((_) {
@@ -130,72 +126,79 @@ class _LargeWindow extends HookConsumerWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          // if (windowSizeMode == WindowSizeMode.large)
-          Row(
-            children: [
-              Expanded(
-                flex: 1,
-                child: Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        channel.invokeMethod(
-                          AppMethodChannel.openOrClosePanel.name,
-                        );
-                      },
-                      icon: const Icon(Icons.close),
-                    ),
-                  ],
-                ),
-              ),
-              // Dragできるようなアイコン
-              const MouseRegion(
-                cursor: SystemMouseCursors.grabbing,
-                child: Icon(Icons.drag_indicator_outlined),
-              ),
-              Expanded(
-                flex: 1,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        channel
-                            .invokeMethod(AppMethodChannel.windowToLeft.name);
-                      },
-                      icon: const Icon(Icons.arrow_circle_left_outlined),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        ref.read(bookmarkControllerProvider.notifier).toggle();
-                      },
-                      tooltip: 'Window does not shrink when inactive',
-                      icon: Icon(
-                        bookmark ? Icons.push_pin : Icons.push_pin_outlined,
-                      ),
-                      color: bookmark
-                          ? context.colorScheme.primary
-                          : context.colorScheme.secondary,
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        channel.invokeMethod(
-                          AppMethodChannel.windowToRight.name,
-                        );
-                      },
-                      icon: const Icon(Icons.arrow_circle_right_outlined),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+          _Header(),
           const TodoList(),
 
           // 初期リリースでは非表示
           // _MemoScreen(),
         ],
       ),
+    );
+  }
+}
+
+class _Header extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final channel = ref.watch(methodChannelProvider);
+    final bookmark = ref.watch(bookmarkControllerProvider);
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  channel.invokeMethod(
+                    AppMethodChannel.openOrClosePanel.name,
+                  );
+                },
+                icon: const Icon(Icons.close),
+              ),
+            ],
+          ),
+        ),
+        // Dragできるようなアイコン
+        const MouseRegion(
+          cursor: SystemMouseCursors.grabbing,
+          child: Icon(Icons.drag_indicator_outlined),
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              IconButton(
+                onPressed: () {
+                  channel.invokeMethod(AppMethodChannel.windowToLeft.name);
+                },
+                icon: const Icon(Icons.arrow_circle_left_outlined),
+              ),
+              IconButton(
+                onPressed: () {
+                  ref.read(bookmarkControllerProvider.notifier).toggle();
+                },
+                tooltip: 'Window does not shrink when inactive',
+                icon: Icon(
+                  bookmark ? Icons.push_pin : Icons.push_pin_outlined,
+                ),
+                color: bookmark
+                    ? context.colorScheme.primary
+                    : context.colorScheme.secondary,
+              ),
+              IconButton(
+                onPressed: () {
+                  channel.invokeMethod(
+                    AppMethodChannel.windowToRight.name,
+                  );
+                },
+                icon: const Icon(Icons.arrow_circle_right_outlined),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
