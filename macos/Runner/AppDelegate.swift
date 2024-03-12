ac3082371bfe2f1c9ec92d734b14108f48bd6a36
imajoriri <imajo.srroni@gmail.com>
2024-03-12 20:59:02 +0900

    docのアプリアイコンを押された時に表示する

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -7,4 +7,12 @@ class AppDelegate: FlutterAppDelegate {
     // falseにすることで、closeされてもバックグラウンドで動き続ける(アプリが終了しない)
     return false
   }
+
+  /// docのアプリアイコンを押された時に表示する
+  override func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+    for window in sender.windows {
+      window.makeKeyAndOrderFront(nil)
+    }
+    return true
+  }
 }
