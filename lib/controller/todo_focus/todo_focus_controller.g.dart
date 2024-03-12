411aee20900443398d222b1383ee18fecebb4871
imajoriri <imajo.srroni@gmail.com>
2024-03-12 12:23:41 +0900

    FocusNodeをproviderで管理する

---
+++ lib/controller/todo_focus/todo_focus_controller.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'todo_focus_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$todoFocusControllerHash() =>
+    r'b4ccaaed3c24c9edea5b95d43bfe23be3ce09856';
+
+/// Todoリストの[FocusNode]を管理するProvider
+///
+/// Copied from [TodoFocusController].
+@ProviderFor(TodoFocusController)
+final todoFocusControllerProvider =
+    AutoDisposeNotifierProvider<TodoFocusController, List<FocusNode>>.internal(
+  TodoFocusController.new,
+  name: r'todoFocusControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$todoFocusControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$TodoFocusController = AutoDisposeNotifier<List<FocusNode>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
