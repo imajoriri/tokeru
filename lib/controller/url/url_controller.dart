143233e4493dab0690e2238f7bff75b02a636ba3
imajoriri <imajo.srroni@gmail.com>
2024-03-20 10:30:56 +0900

    URL調整

--- lib/controller/url/url_controller.dart
+++ lib/controller/url/url_controller.dart
@@ -5,11 +5,18 @@ enum UrlController {
   /// 開発者のXアカウント
   developerXAccount("https://twitter.com/imasirooo"),
 
-  /// フィードバックURL
-  feedback(''),
+  /// 機能リクエスト
+  featureRequest(
+    'https://github.com/tinp-lab/tokeru/issues/new?template=feature_request.md',
+  ),
+
+  /// バグ報告
+  bugReport(
+    'https://github.com/tinp-lab/tokeru/issues/new?template=bug_report.md',
+  ),
 
   /// TokeruのGitHub Repository
-  tokeruRepository(''),
+  tokeruRepository('https://github.com/tinp-lab/tokeru'),
   ;
 
   const UrlController(this.url);
