7022aae2f78204b134cbf2685997fe8e01661dea
imajoriri <imajo.srroni@gmail.com>
2024-04-06 22:20:14 +0900

    Todoの一番下で↓を押した時にTextFieldにフォーカスを移動する

--- lib/model/memo/memo.freezed.dart
+++ lib/model/memo/memo.freezed.dart
@@ -12,7 +12,7 @@ part of 'memo.dart';
 T _$identity<T>(T value) => value;
 
 final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
 /// @nodoc
 mixin _$Memo {
@@ -99,7 +99,7 @@ class _$MemoImpl implements _Memo {
   }
 
   @override
-  bool operator ==(dynamic other) {
+  bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _$MemoImpl &&
