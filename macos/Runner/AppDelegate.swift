5d43b561eadb9dd8f91704a6aea7d4b7f08d8eac
imajoriri <imajo.srroni@gmail.com>
2024-04-14 15:39:08 +0900

    リファクタ

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -8,23 +8,17 @@ class AppDelegate: FlutterAppDelegate {
     return false
   }
 
-  /// docのアプリアイコンを押された時に表示する
-  override func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
-    for window in sender.windows {
-      // 一度orderOutすることでステージマネージャーを使っている際に
-      // 他のアプリケーションが隠れることを防いでいる。
-      // 本来はapplicationShouldHandleReopenのreturnをfalseにすれば実現できるはずだがうまくいかないので
-      // 代替案を使っている
-      window.orderOut(nil)
-      window.makeKeyAndOrderFront(nil)
-    }
-    return false
-  }
-
   // accessoryにすることで、menu barが表示されない
   override func applicationDidFinishLaunching(_ notification: Notification) {
     // menu barあった方がショートカットキーを学習させられるので一旦コメントアウトする
     // 思い出しやすいようにコード自体は残す
     // NSApp.setActivationPolicy(.accessory)
+  }
+
+  // Dockやアプリケーションスイッチャーで選択された時にウィンドウを表示する
+  override func applicationDidBecomeActive(_ notification: Notification) {
+    for window in NSApp.windows {
+      window.makeKeyAndOrderFront(nil)
+    }
   }
 }
