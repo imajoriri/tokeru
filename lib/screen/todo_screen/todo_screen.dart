0f95cd7338513eb2209d52f03255085f8fc9a56d
imajoriri <imajo.srroni@gmail.com>
2024-04-30 20:49:37 +0900

    umimport

--- lib/screen/todo_screen/todo_screen.dart
+++ lib/screen/todo_screen/todo_screen.dart
@@ -12,7 +12,6 @@ import 'package:quick_flutter/controller/todo_text_editing_controller/todo_text_
 import 'package:quick_flutter/controller/todo_text_field_focus/todo_text_field_focus_controller.dart';
 import 'package:quick_flutter/model/analytics_event/analytics_event_name.dart';
 import 'package:quick_flutter/model/todo/todo.dart';
-import 'package:quick_flutter/systems/context_extension.dart';
 import 'package:quick_flutter/widget/actions/delete_todo/delete_todo_action.dart';
 import 'package:quick_flutter/widget/actions/focus_down/focus_down_action.dart';
 import 'package:quick_flutter/widget/actions/focus_up/focus_up_action.dart';
