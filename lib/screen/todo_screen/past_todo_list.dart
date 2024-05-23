0c5127c0aaaa58aef2dfb7a83753743e6583c3ca
imajoriri <imajo.srroni@gmail.com>
2024-05-24 07:40:34 +0900

    past todoを昨日とそれ以外に分ける

--- lib/screen/todo_screen/past_todo_list.dart
+++ lib/screen/todo_screen/past_todo_list.dart
@@ -12,19 +12,55 @@ class PastTodoList extends HookConsumerWidget {
         if (todos.isEmpty) {
           return const Text('No past todos');
         }
+        final yesterday = DateTime.now().subtract(const Duration(days: 1));
+        final yesterdayStart =
+            DateTime(yesterday.year, yesterday.month, yesterday.day);
+        // 機能のTodoListItem
+        final yesterdayTodo = todos.where((todo) {
+          return todo.createdAt.isAfter(yesterdayStart);
+        }).toList();
+
+        final notYesterdayTodo = todos.where((todo) {
+          return todo.createdAt.isBefore(yesterdayStart);
+        }).toList();
+
         return ExpansionTile(
           title: const Text('Past Todos'),
-          children: List.generate(todos.length, (index) {
-            final todo = todos[index];
-            return switch (todo) {
-              Todo() => TodoListItem(
-                  todo: todo,
-                  controller: TextEditingController(text: todo.title),
-                  readOnly: true,
-                ),
-              TodoDivider() => throw UnimplementedError(),
-            };
-          }),
+          expandedAlignment: Alignment.topLeft,
+          children: [
+            Text(
+              'Yesterday',
+              style: context.appTextTheme.headlineMedium,
+            ),
+            Column(
+              children: List.generate(yesterdayTodo.length, (index) {
+                final todo = yesterdayTodo[index];
+                return switch (todo) {
+                  Todo() => TodoListItem(
+                      todo: todo,
+                      controller: TextEditingController(text: todo.title),
+                      readOnly: true,
+                    ),
+                  TodoDivider() => throw UnimplementedError(),
+                };
+              }),
+            ),
+            const SizedBox(height: 8),
+            Text('Older', style: context.appTextTheme.headlineMedium),
+            Column(
+              children: List.generate(notYesterdayTodo.length, (index) {
+                final todo = notYesterdayTodo[index];
+                return switch (todo) {
+                  Todo() => TodoListItem(
+                      todo: todo,
+                      controller: TextEditingController(text: todo.title),
+                      readOnly: true,
+                    ),
+                  TodoDivider() => throw UnimplementedError(),
+                };
+              }),
+            ),
+          ],
         );
       },
       error: (e, s) => const Text('happen somethings'),
