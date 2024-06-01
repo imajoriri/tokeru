a8e42e792d8bd1c8f404be998184e9686aceebff
imajoriri <imajo.srroni@gmail.com>
2024-06-01 13:48:28 +0900

    UI修正

--- lib/screen/todo_screen/past_todo_list.dart
+++ lib/screen/todo_screen/past_todo_list.dart
@@ -3,6 +3,21 @@ part of 'todo_screen.dart';
 class PastTodoList extends HookConsumerWidget {
   const PastTodoList({super.key});
 
+  /// 日付けごとにグルーピングするメソッド
+  Map<DateTime, List<TodoItem>> groupByDate(List<TodoItem> todoItems) {
+    final grouped = <DateTime, List<TodoItem>>{};
+    for (final item in todoItems) {
+      // createdAtの時間を切り捨てた日付を取得
+      final date = DateTime(
+          item.createdAt.year, item.createdAt.month, item.createdAt.day);
+      if (!grouped.containsKey(date)) {
+        grouped[date] = [];
+      }
+      grouped[date]!.add(item);
+    }
+    return grouped;
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final todos = ref.watch(pastTodoControllerProvider);
@@ -12,17 +27,9 @@ class PastTodoList extends HookConsumerWidget {
         if (todos.isEmpty) {
           return const SizedBox();
         }
-        final yesterday = DateTime.now().subtract(const Duration(days: 1));
-        final yesterdayStart =
-            DateTime(yesterday.year, yesterday.month, yesterday.day);
-        // 機能のTodoListItem
-        final yesterdayTodo = todos.where((todo) {
-          return todo.createdAt.isAfter(yesterdayStart);
-        }).toList();
 
-        final notYesterdayTodo = todos.where((todo) {
-          return todo.createdAt.isBefore(yesterdayStart);
-        }).toList();
+        final now = DateTime.now();
+        final today = DateTime(now.year, now.month, now.day);
 
         return ListTileTheme.merge(
           dense: true,
@@ -32,51 +39,40 @@ class PastTodoList extends HookConsumerWidget {
             title: Text('Past Todos', style: context.appTextTheme.titleSmall),
             controlAffinity: ListTileControlAffinity.leading,
             expandedCrossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(16, 12, 16, 4),
-                child: Text(
-                  'Yesterday',
-                  style: context.appTextTheme.titleSmall
-                      .copyWith(color: context.appColors.textSubtle),
-                ),
-              ),
-              Column(
-                children: List.generate(yesterdayTodo.length, (index) {
-                  final todo = yesterdayTodo[index];
-                  return switch (todo) {
-                    Todo() => TodoListItem(
-                        todo: todo,
-                        controller: TextEditingController(text: todo.title),
-                        readOnly: true,
-                      ),
-                    TodoDivider() => throw UnimplementedError(),
-                  };
-                }),
-              ),
-              const SizedBox(height: 8),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(16, 12, 16, 4),
-                child: Text(
-                  'Before yesterday',
-                  style: context.appTextTheme.titleSmall
-                      .copyWith(color: context.appColors.textSubtle),
-                ),
-              ),
-              Column(
-                children: List.generate(notYesterdayTodo.length, (index) {
-                  final todo = notYesterdayTodo[index];
-                  return switch (todo) {
-                    Todo() => TodoListItem(
-                        todo: todo,
-                        controller: TextEditingController(text: todo.title),
-                        readOnly: true,
-                      ),
-                    TodoDivider() => throw UnimplementedError(),
-                  };
-                }),
-              ),
-            ],
+            children: groupByDate(todos).entries.map((entry) {
+              final date = entry.key;
+              final todos = entry.value;
+              if (todos.isEmpty) {
+                return const SizedBox();
+              }
+              // 今日との差分を取得
+              final diff = today.difference(date).inDays;
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Text(
+                      "${DateFormat.yMMMd().format(date)} ($diff days ago)",
+                      style: context.appTextTheme.titleSmall,
+                    ),
+                  ),
+                  Column(
+                    children: List.generate(todos.length, (index) {
+                      final todo = todos[index];
+                      return switch (todo) {
+                        Todo() => TodoListItem(
+                            todo: todo,
+                            controller: TextEditingController(text: todo.title),
+                            readOnly: true,
+                          ),
+                        TodoDivider() => throw UnimplementedError(),
+                      };
+                    }),
+                  ),
+                ],
+              );
+            }).toList(),
           ),
         );
       },
