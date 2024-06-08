4748ea6fbb49c5cec466433594dd0889e18b0778
imajoriri <imajo.srroni@gmail.com>
2024-06-08 15:44:08 +0900

    fix: ChatのTodoIdが変わってもTextFieldがリビルドされないようにする

--- lib/screen/todo_screen/chat/chat_view.dart
+++ lib/screen/todo_screen/chat/chat_view.dart
@@ -6,7 +6,7 @@ import 'package:quick_flutter/controller/chat/chat_controller.dart';
 import 'package:quick_flutter/controller/selected_todo_item/selected_todo_item_controller.dart';
 import 'package:quick_flutter/widget/focus_nodes.dart';
 
-class ChatView extends ConsumerWidget {
+class ChatView extends HookConsumerWidget {
   const ChatView({super.key});
 
   @override
@@ -15,59 +15,46 @@ class ChatView extends ConsumerWidget {
     if (todoId == null) {
       return const SizedBox();
     }
-
+    final provider = chatControllerProvider(todoId);
     final chats = ref.watch(chatControllerProvider(todoId));
-    return chats.when(
-      data: (chats) {
-        return const _ChatListView();
-      },
-      loading: () {
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      },
-      error: (error, _) {
-        return Center(
-          child: Text('Error: $error'),
-        );
-      },
-    );
-  }
-}
-
-class _ChatListView extends HookConsumerWidget {
-  const _ChatListView();
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final todoId = ref.watch(selectedTodoItemIdControllerProvider);
-    final provider = chatControllerProvider(todoId!);
-    final chats = ref.watch(provider).requireValue;
-
     final textEditingController = useTextEditingController();
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          Expanded(
-            child: ListView.separated(
-              itemCount: chats.length,
-              shrinkWrap: true,
-              reverse: true,
-              separatorBuilder: (context, index) {
-                return const SizedBox(height: 8);
-              },
-              itemBuilder: (context, index) {
-                final chat = chats[index];
-                return Container(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-                  color: Colors.grey[100],
-                  child: Text(chat.body),
-                );
-              },
-            ),
+          chats.when(
+            data: (chats) {
+              return Expanded(
+                child: ListView.separated(
+                  itemCount: chats.length,
+                  shrinkWrap: true,
+                  reverse: true,
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(height: 8);
+                  },
+                  itemBuilder: (context, index) {
+                    final chat = chats[index];
+                    return Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: 8),
+                      color: Colors.grey[100],
+                      child: Text(chat.body),
+                    );
+                  },
+                ),
+              );
+            },
+            loading: () {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+            error: (error, _) {
+              return Center(
+                child: Text('Error: $error'),
+              );
+            },
           ),
           CallbackShortcuts(
             bindings: <ShortcutActivator, VoidCallback>{
