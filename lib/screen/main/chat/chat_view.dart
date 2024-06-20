0c15828e10c96ad99446901f0505b13e6ac49645
imajoriri <imajo.srroni@gmail.com>
2024-06-21 08:20:07 +0900

    refactor: Update vertical divider width in MainScreen

--- lib/screen/main/chat/chat_view.dart
+++ lib/screen/main/chat/chat_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -86,8 +87,9 @@ class ChatView extends HookConsumerWidget {
             );
           },
           loading: () {
-            return const Center(
-              child: CircularProgressIndicator(),
+            return const Padding(
+              padding: EdgeInsets.all(20.0),
+              child: CupertinoActivityIndicator(),
             );
           },
           error: (error, _) {
@@ -155,6 +157,7 @@ class _ChatTextField extends HookConsumerWidget {
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
             child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 Expanded(
                   child: TextField(
@@ -166,7 +169,7 @@ class _ChatTextField extends HookConsumerWidget {
                     decoration: const InputDecoration(
                       isDense: true,
                       border: InputBorder.none,
-                      hintText: 'Type a message',
+                      hintText: 'Talk to myself',
                     ),
                   ),
                 ),
