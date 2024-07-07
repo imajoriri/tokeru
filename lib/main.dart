7ec58968ec16bc0ae9958753ffa86ca06464a578
imajoriri <imajo.srroni@gmail.com>
2024-07-07 18:57:36 +0900

    main.dartのリファクタ

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -7,18 +6,12 @@ import 'package:hotkey_manager/hotkey_manager.dart';
 import 'package:logger/logger.dart';
 import 'package:quick_flutter/controller/hot_key/hot_key_controller.dart';
 import 'package:quick_flutter/controller/refresh/refresh_controller.dart';
-import 'package:quick_flutter/controller/screen_type/screen_type_controller.dart';
 import 'package:quick_flutter/firebase_options.dart';
-import 'package:quick_flutter/model/analytics_event/analytics_event_name.dart';
 import 'package:quick_flutter/screen/main/main_screen.dart';
 import 'package:quick_flutter/screen/panel/panel_screen.dart';
-import 'package:quick_flutter/screen/settings/settings_screen.dart';
 import 'package:quick_flutter/utils/method_channel.dart';
-import 'package:quick_flutter/widget/actions/new_todo.dart/new_todo_action.dart';
-import 'package:quick_flutter/widget/actions/reload/reload_action.dart';
-import 'package:quick_flutter/widget/actions/toggle_focus/toggle_focus_action.dart';
 import 'package:quick_flutter/widget/app_platform_menu_bar.dart';
-import 'package:quick_flutter/widget/shortcutkey.dart';
+import 'package:quick_flutter/widget/callback_shortcut.dart';
 import 'package:quick_flutter/widget/theme/app_theme.dart';
 
 @pragma('vm:entry-point')
@@ -48,12 +41,14 @@ void main() async {
     ProviderScope(
       observers: [_AppObserver()],
       child: AppMaterialApp(
-        home: _CallbackShortcuts(
+        home: AppCallbackShortcuts(
           child: AppPlatformMenuBar(
             child: Consumer(
               builder: (context, ref, child) {
-                final largeWindowKey = GlobalKey();
-                ref.watch(hotKeyControllerProvider);
+                // 画面表示時にホットキーを登録するためにProviderを参照する。
+                // リビルドされたいわけではないのでreadを使う。
+                ref.read(hotKeyControllerProvider);
+
                 mainMethodChannel.addListnerActive((_) async {
                   if (ref.read(refreshControllerProvider.notifier).isPast()) {
                     ref.invalidate(refreshControllerProvider);
@@ -62,9 +57,7 @@ void main() async {
 
                 return Material(
                   color: context.appColors.backgroundDefault,
-                  child: _LargeWindow(
-                    key: largeWindowKey,
-                  ),
+                  child: const MainScreen(),
                 );
               },
             ),
@@ -73,71 +66,6 @@ void main() async {
       ),
     ),
   );
-}
-
-class _LargeWindow extends HookConsumerWidget {
-  const _LargeWindow({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final screenType = ref.watch(screenTypeControllerProvider);
-
-    return switch (screenType) {
-      ScreenType.todo => const MainScreen(),
-      ScreenType.settings => const SettingsScreen(),
-    };
-  }
-}
-
-/// [CallbackShortcuts]の中でRefを使うためにラップしたWidgetクラス
-class _CallbackShortcuts extends ConsumerWidget {
-  const _CallbackShortcuts({required this.child});
-  final Widget child;
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return FocusScope(
-      child: Shortcuts(
-        shortcuts: {
-          ShortcutActivatorType.newTodo.shortcutActivator:
-              const NewTodoIntent(),
-          ShortcutActivatorType.reload.shortcutActivator: const ReloadIntent(),
-          ShortcutActivatorType.switchFocusChat.shortcutActivator:
-              const ToggleFocusIntent(),
-          ShortcutActivatorType.switchFocusTodo.shortcutActivator:
-              const ToggleFocusIntent(),
-        },
-        child: Actions(
-          dispatcher: _LoggingActionDispatcher(),
-          actions: {
-            NewTodoIntent: ref.read(newTodoActionProvider),
-            ReloadIntent: ref.read(reloadActionProvider),
-            ToggleFocusIntent: ref.read(toggleFocusActionProvider),
-          },
-          child: child,
-        ),
-      ),
-    );
-  }
-}
-
-class _LoggingActionDispatcher extends ActionDispatcher {
-  @override
-  (bool, Object?) invokeActionIfEnabled(
-    covariant Action<Intent> action,
-    covariant Intent intent, [
-    BuildContext? context,
-  ]) {
-    if (action is! DoNothingAction) {
-      FirebaseAnalytics.instance.logEvent(
-        name: AnalyticsEventName.tapShortcutKey.name,
-        parameters: {
-          'action': action.runtimeType.toString(),
-        },
-      );
-    }
-
-    return super.invokeActionIfEnabled(action, intent, context);
-  }
 }
 
 class _AppObserver extends ProviderObserver {
