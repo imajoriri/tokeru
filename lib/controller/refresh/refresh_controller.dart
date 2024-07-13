3e341462dabfe99aba96b23199d4ef962a6542e5
imajoriri <imajo.srroni@gmail.com>
2024-07-13 18:04:16 +0900

    ウィンドウをアクティブになったらchatを更新する

--- lib/controller/refresh/refresh_controller.dart
+++ lib/controller/refresh/refresh_controller.dart
@@ -2,8 +2,10 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'refresh_controller.g.dart';
 
-/// データを更新するためのコントローラー。
+/// アプリ内のデータを更新するためのコントローラー。
 ///
+/// 「Cmd + R」でこのProviderがinvalidateされ、watchしているProviderも更新されることを
+/// 想定している。
 /// 最新更新日を返す。
 /// このコントローラーをref.watchすることで、データの更新を行う。
 @Riverpod(keepAlive: true)
@@ -11,12 +13,5 @@ class RefreshController extends _$RefreshController {
   @override
   DateTime build() {
     return DateTime.now();
-  }
-
-  /// 更新が昨日以前のものかどうかを判定する
-  bool isPast() {
-    final now = DateTime.now();
-    final todayStart = DateTime(now.year, now.month, now.day);
-    return state.isBefore(todayStart);
   }
 }
