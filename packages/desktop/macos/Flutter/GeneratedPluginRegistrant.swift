fb7ae5770eca05f801539cfb3429a62ddc993e31
imajoriri <imajo.srroni@gmail.com>
2024-07-26 09:48:42 +0900

    chore: Remove unused desktop_window plugin from Linux and Windows generated_plugins.cmake

--- packages/desktop/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/desktop/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,12 +6,10 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
-import desktop_window
 import firebase_analytics
 import firebase_auth
 import firebase_core
 import firebase_crashlytics
-import google_sign_in_ios
 import hotkey_manager_macos
 import path_provider_foundation
 import shared_preferences_foundation
@@ -20,12 +18,10 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  DesktopWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWindowPlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
-  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   HotkeyManagerMacosPlugin.register(with: registry.registrar(forPlugin: "HotkeyManagerMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
