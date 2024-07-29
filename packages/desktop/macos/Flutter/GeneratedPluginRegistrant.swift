fe8f8fc91cb3ee60e6dfa768f464e9bbf0005c83
imajoriri <imajo.srroni@gmail.com>
2024-07-29 16:00:48 +0900

    apple sign in

--- packages/desktop/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/desktop/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,6 +14,7 @@ import google_sign_in_ios
 import hotkey_manager_macos
 import path_provider_foundation
 import shared_preferences_foundation
+import sign_in_with_apple
 import sqflite
 import url_launcher_macos
 
@@ -27,6 +28,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   HotkeyManagerMacosPlugin.register(with: registry.registrar(forPlugin: "HotkeyManagerMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
