ade1af5c6538acfd6cd5b3ee8c20ea9d0457df66
imajoriri <imajo.srroni@gmail.com>
2024-07-29 16:00:48 +0900

    apple sign in

--- packages/model/lib/controller/user/user_controller.dart
+++ packages/model/lib/controller/user/user_controller.dart
@@ -88,6 +88,38 @@ class UserController extends _$UserController {
     }
   }
 
+  /// Apple sign in でログインする。
+  Future<void> signInWithApple() async {
+    try {
+      await auth.FirebaseAuth.instance.currentUser
+          ?.linkWithProvider(auth.AppleAuthProvider());
+    } on auth.FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case "provider-already-linked":
+          // すでにlink済みの場合、`linkWithProvider`でエラーになる。
+          // その場合には、`signInWithCredential`でログインするとエラーになるため、
+          // `signInWithProvider`を使うが、ログインダイアログが2回出てしまうため、いずれ改善したい。
+          await auth.FirebaseAuth.instance
+              .signInWithProvider(auth.AppleAuthProvider());
+          Exception("The provider has already been linked to the user.");
+          break;
+        case "invalid-credential":
+          Exception("The provider's credential is not valid.");
+          break;
+        case "credential-already-in-use":
+          Exception(
+              "The account corresponding to the credential already exists, "
+              "or is already linked to a Firebase User.");
+          break;
+        // See the API reference for the full list of error codes.
+        default:
+          Exception("Unknown error.");
+      }
+    } finally {
+      ref.invalidateSelf();
+    }
+  }
+
   /// サインアウトする。
   Future<void> signOut() async {
     await auth.FirebaseAuth.instance.signOut();
