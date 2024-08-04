34095c79c61026ea10b10024be71a6a761f72ddc
imajoriri <imajo.srroni@gmail.com>
2024-08-04 13:57:19 +0900

    Riverpod3にあげる

--- packages/model/lib/controller/read/read_controller.g.dart
+++ packages/model/lib/controller/read/read_controller.g.dart
@@ -6,7 +6,7 @@ part of 'read_controller.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$readControllerHash() => r'4574c4c6fd266c2c8b4d65f6452d8a35e5668b57';
+String _$readControllerHash() => r'9fa220a52160ece9a5a034b00b341ecf339486b5';
 
 /// Todoの既読した時刻を管理するコントローラー。
 ///
@@ -25,4 +25,4 @@ final readControllerProvider =
 
 typedef _$ReadController = AutoDisposeStreamNotifier<DateTime?>;
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, inference_failure_on_uninitialized_variable, inference_failure_on_function_return_type, inference_failure_on_untyped_parameter, deprecated_member_use_from_same_package
