2128d42227423e2a9583e7d0db3fc1feca98f0c1
imajoriri <imajo.srroni@gmail.com>
2024-08-28 08:11:08 +0900

    remove focus scope

--- packages/desktop/lib/screen/main/thread/sub_todo.dart
+++ packages/desktop/lib/screen/main/thread/sub_todo.dart
@@ -38,100 +38,96 @@ class _SubTodoView extends HookConsumerWidget {
             ),
             child: subTodos.when(
               data: (todos) {
-                final focusScope = FocusScopeNode();
-                return FocusScope(
-                  node: focusScope,
-                  child: CustomScrollView(
-                    slivers: [
-                      SliverList(
-                        delegate: SliverChildListDelegate(
-                          [
-                            ReorderableListView.builder(
-                              buildDefaultDragHandles: false,
-                              physics: const NeverScrollableScrollPhysics(),
-                              shrinkWrap: true,
-                              itemCount: todos.length,
-                              onReorder: (oldIndex, newIndex) {
-                                // NOTE: なぜか上から下に移動するときはnewIndexが1つずれるので
-                                // その分を補正する
-                                if (oldIndex < newIndex) {
-                                  newIndex -= 1;
-                                }
-                                ref
-                                    .read(provider.notifier)
-                                    .reorder(oldIndex, newIndex);
-                              },
-                              itemBuilder: (context, index) {
-                                final key = ValueKey(todos[index].id);
-                                final todo = todos[index];
-                                return HookBuilder(
-                                  key: key,
-                                  builder: (context) {
-                                    final focusNode = useFocusNode();
-                                    if (currentFocusIndex.value == index) {
-                                      WidgetsBinding.instance
-                                          .addPostFrameCallback((_) {
-                                        focusNode.requestFocus();
-                                        currentFocusIndex.value = null;
-                                      });
-                                    }
-                                    return TodoListItem(
-                                      isDone: todo.isDone,
-                                      index: index,
-                                      title: todo.title,
-                                      focusNode: focusNode,
-                                      onDeleted: () async {
-                                        ref
-                                            .read(provider.notifier)
-                                            .delete(todoId: todo.id);
-                                      },
-                                      onUpdatedTitle: (value) {
-                                        ref.read(provider.notifier).updateTitle(
-                                            todoId: todo.id, title: value);
-                                      },
-                                      onToggleDone: (value) {
-                                        ref
-                                            .read(provider.notifier)
-                                            .toggleDone(todoId: todo.id);
-                                      },
-                                      focusDown: () {
-                                        FocusScope.of(context).nextFocus();
-                                      },
-                                      focusUp: () {
-                                        FocusScope.of(context).previousFocus();
-                                      },
-                                      onNewTodoBelow: () async {
-                                        await ref
-                                            .read(provider.notifier)
-                                            .addWithIndex(index + 1);
-                                        currentFocusIndex.value = index + 1;
-                                      },
-                                      // 一番上のTodoは上に移動できない
-                                      onSortUp: index != 0
-                                          ? () {
-                                              ref
-                                                  .read(provider.notifier)
-                                                  .reorder(index, index - 1);
-                                            }
-                                          : null,
-                                      // 一番下のTodoは下に移動できない
-                                      onSortDown: index != todos.length - 1
-                                          ? () {
-                                              ref
-                                                  .read(provider.notifier)
-                                                  .reorder(index, index + 1);
-                                            }
-                                          : null,
-                                    );
-                                  },
-                                );
-                              },
-                            ),
-                          ],
-                        ),
+                return CustomScrollView(
+                  slivers: [
+                    SliverList(
+                      delegate: SliverChildListDelegate(
+                        [
+                          ReorderableListView.builder(
+                            buildDefaultDragHandles: false,
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: todos.length,
+                            onReorder: (oldIndex, newIndex) {
+                              // NOTE: なぜか上から下に移動するときはnewIndexが1つずれるので
+                              // その分を補正する
+                              if (oldIndex < newIndex) {
+                                newIndex -= 1;
+                              }
+                              ref
+                                  .read(provider.notifier)
+                                  .reorder(oldIndex, newIndex);
+                            },
+                            itemBuilder: (context, index) {
+                              final key = ValueKey(todos[index].id);
+                              final todo = todos[index];
+                              return HookBuilder(
+                                key: key,
+                                builder: (context) {
+                                  final focusNode = useFocusNode();
+                                  if (currentFocusIndex.value == index) {
+                                    WidgetsBinding.instance
+                                        .addPostFrameCallback((_) {
+                                      focusNode.requestFocus();
+                                      currentFocusIndex.value = null;
+                                    });
+                                  }
+                                  return TodoListItem(
+                                    isDone: todo.isDone,
+                                    index: index,
+                                    title: todo.title,
+                                    focusNode: focusNode,
+                                    onDeleted: () async {
+                                      ref
+                                          .read(provider.notifier)
+                                          .delete(todoId: todo.id);
+                                    },
+                                    onUpdatedTitle: (value) {
+                                      ref.read(provider.notifier).updateTitle(
+                                          todoId: todo.id, title: value);
+                                    },
+                                    onToggleDone: (value) {
+                                      ref
+                                          .read(provider.notifier)
+                                          .toggleDone(todoId: todo.id);
+                                    },
+                                    focusDown: () {
+                                      FocusScope.of(context).nextFocus();
+                                    },
+                                    focusUp: () {
+                                      FocusScope.of(context).previousFocus();
+                                    },
+                                    onNewTodoBelow: () async {
+                                      await ref
+                                          .read(provider.notifier)
+                                          .addWithIndex(index + 1);
+                                      currentFocusIndex.value = index + 1;
+                                    },
+                                    // 一番上のTodoは上に移動できない
+                                    onSortUp: index != 0
+                                        ? () {
+                                            ref
+                                                .read(provider.notifier)
+                                                .reorder(index, index - 1);
+                                          }
+                                        : null,
+                                    // 一番下のTodoは下に移動できない
+                                    onSortDown: index != todos.length - 1
+                                        ? () {
+                                            ref
+                                                .read(provider.notifier)
+                                                .reorder(index, index + 1);
+                                          }
+                                        : null,
+                                  );
+                                },
+                              );
+                            },
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 );
               },
               loading: () {
